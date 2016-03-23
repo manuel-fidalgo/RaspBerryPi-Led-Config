8cc75ae0fbea7374dcf001c91964426bc7622788
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-03-23 22:21:42 +0100

    finished project, no tested

--- src/SecondWindow.java
+++ src/SecondWindow.java
@@ -2,23 +2,33 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 @SuppressWarnings("serial")
-public class SecondWindow extends JFrame {
+public class SecondWindow extends JFrame{
 
-	final int RED = 17;
-	final int GREEN = 23;
-	final int BLUE = 24;
+	int RED = 17;
+	int GREEN = 23;
+	int BLUE = 24;
 
 	private JPanel contentPane;
-	private  String colores[] = {"Negro","Azul","Verde","Turquesa","Rojo","Morado","Amarillo","Blanco","Apagado"};
-	private  Color colores_[] = {Color.BLACK,Color.BLUE,Color.GREEN, new Color(93, 193, 185),Color.RED, new Color(207,91,148),Color.YELLOW,Color.WHITE,Color.LIGHT_GRAY};
+	private  String colores[] = {"Black","Blue","Green","Turquoise","Red","Purple","Yellow","White","Exit"};
+	private  Color colores_[] = {
+								 new Color(0x000000),new Color(0x408AD2),new Color(0x34E444),
+								 new Color(0x35D4A4), new Color(0xFF4040), new Color(0xD636C9),
+								 new Color(0xFFEB40), new Color(0xFFFFFF) , new Color(0x797A57)
+								 };
 
 	/**
 	 * Launch the application.
@@ -40,47 +50,116 @@ public class SecondWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public SecondWindow() {
+		setTitle("Color led manager.");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 300, 300);
+		setResizable(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.WHITE);
+		setJMenuBar(menuBar);
+		
+		JMenu mnCambiarPuertos = new JMenu("Change ports.");
+		mnCambiarPuertos.setBackground(Color.WHITE);
+		menuBar.add(mnCambiarPuertos);
+		
+		JMenuItem mntmPuertoLedRojo = new JMenuItem("Change red port.");
+		mntmPuertoLedRojo.setBackground(Color.WHITE);
+		mntmPuertoLedRojo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String s = JOptionPane.showInputDialog("Red port:");
+				System.out.println(s);
+				try{
+					RED = Integer.parseInt(s);
+				}catch(Exception ex){
+					JOptionPane.showMessageDialog(null, "Is not a valid port.");
+				}
+			}
+		});
+		mnCambiarPuertos.add(mntmPuertoLedRojo);
+		
+		JMenuItem mntmPuertoLedVerde = new JMenuItem("Change green port.");
+		mntmPuertoLedVerde.setBackground(Color.WHITE);
+		mntmPuertoLedVerde.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String s = JOptionPane.showInputDialog("Green Port:");
+				System.out.println(s);
+				try{
+					GREEN = Integer.parseInt(s);
+				}catch(Exception ex){
+					JOptionPane.showMessageDialog(null, "Is not a valid port.");
+				}
+			}
+		});
+		mnCambiarPuertos.add(mntmPuertoLedVerde);
+		
+		JMenuItem mntmPuertoLedAzul = new JMenuItem("Change blue port.");
+		mntmPuertoLedAzul.setBackground(Color.WHITE);
+		mntmPuertoLedAzul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String s = JOptionPane.showInputDialog("Blue Port:");
+				System.out.println(s);
+				try{
+					BLUE = Integer.parseInt(s);
+				}catch(Exception ex){
+					JOptionPane.showMessageDialog(null, "Is not a valid port.");
+				}
+			}
+		});
+		mnCambiarPuertos.add(mntmPuertoLedAzul);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmShowHelp = new JMenuItem("Show help");
+		mntmShowHelp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,"No help LOL");
+			}
+		});
+		mnHelp.add(mntmShowHelp);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	//	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		JPanel panel_1 = new JPanel();
 		this.getContentPane().add(panel_1);
 		GridLayout gbl_panel_1 = new GridLayout(3,3,0,0);
 		panel_1.setLayout(gbl_panel_1);
-		//		String colores[] = {"Negro","Azul","Verde","Turquesa","Rojo","Morado","Amarillo","Blanco","Apagado"};
-		//		Color colores_[] = {Color.BLACK,Color.BLUE,Color.GREEN, new Color(93, 193, 185),Color.RED, new Color(207,91,148),Color.YELLOW,Color.WHITE,Color.LIGHT_GRAY};
 		ActionListener al = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JButton boton = (JButton)e.getSource();
 				String s = boton.getText();
-				if(s.equalsIgnoreCase(colores[0])){
-					black();
-				}else if(s.equalsIgnoreCase(colores[1])){
-					blue();
-				}else if(s.equalsIgnoreCase(colores[2])){
-					green();
-				}else if(s.equalsIgnoreCase(colores[3])){
-					turquoise();
-				}else if(s.equalsIgnoreCase(colores[4])){
-					red();
-				}else if(s.equalsIgnoreCase(colores[5])){
-					purple();
-				}else if(s.equalsIgnoreCase(colores[6])){
-					yellow();
-				}else if(s.equalsIgnoreCase(colores[7])){
-					white();
-				}else{
-					off();
+				try{
+					if(s.equalsIgnoreCase(colores[0])){
+						black();
+					}else if(s.equalsIgnoreCase(colores[1])){
+						blue();
+					}else if(s.equalsIgnoreCase(colores[2])){
+						green();
+					}else if(s.equalsIgnoreCase(colores[3])){
+						turquoise();
+					}else if(s.equalsIgnoreCase(colores[4])){
+						red();
+					}else if(s.equalsIgnoreCase(colores[5])){
+						purple();
+					}else if(s.equalsIgnoreCase(colores[6])){
+						yellow();
+					}else if(s.equalsIgnoreCase(colores[7])){
+						white();
+					}else{
+						off();
+					}
+				}catch(IOException ex){
+					JOptionPane.showMessageDialog(null,"Runtime error: "+ex.getMessage());
 				}
 			}
 		};
 		for (int i = 0; i < 9; i++) {
 			JButton b = new JButton(colores[i]);
 			b.addActionListener(al);
+			b.setBorder(new LineBorder(Color.white,0));
 			b.setBackground(colores_[i]);
 			if(i==0) b.setForeground(Color.WHITE);
 			panel_1.add(b);
@@ -89,48 +168,57 @@ public class SecondWindow extends JFrame {
 
 	}
 	/*Apaga ambos leds*/
-	public void black(){
-		System.out.println("Test");
+	//echo 1 > /sys/class/gpio/gpio17/value
+	public void black() throws IOException{
+		execCommand(0,RED);
+		execCommand(0,BLUE);
+		execCommand(0,GREEN);
 	}
+
 	/*Enciende el led Azul*/
-	public void blue(){
+	public void blue() throws IOException{
 		black();
+		execCommand(1,BLUE);
 
 	}
 	/*Enciende el led Verde*/
-	public void green(){
+	public void green() throws IOException {
 		black();
-
+		execCommand(1,GREEN);
 	}
 	/*Enciende el led Rojo*/
-	public void red(){
+	public void red() throws IOException{
 		black();
+		execCommand(1,RED);
+	}
+	public void turquoise() throws IOException{
+		black();
+		execCommand(1,BLUE);
+		execCommand(1,GREEN);
+	}
 
-	}
-	public void turquoise(){
+	public void purple() throws IOException{
 		black();
-		blue();
-		green();
+		execCommand(1,BLUE);
+		execCommand(1,RED);
 	}
-
-	public void purple(){
+	public void yellow() throws IOException{
 		black();
-		red();
-		blue();
+		execCommand(1,RED);
+		execCommand(1,GREEN);
 	}
-	public void yellow(){
-		black();
-		green();
-		red();
+	public void white()throws IOException{
+		execCommand(1,RED);
+		execCommand(1,GREEN);
+		execCommand(1,BLUE);
 	}
-	public void white(){
-		black();
-		red();
-		green();
-		blue();
-	}
-	public void off(){
+	public void off() throws IOException{
 		System.exit(0);
+	}
+	private void  execCommand(int status, int puerto) throws IOException{
+		String command = "echo "+Integer.toString(status)+" > /sys/class/gpio/gpio"+Integer.toString(puerto)+"/value";
+		System.out.println(command);
+		Runtime.getRuntime().exec(command);
 	}
 
 }
